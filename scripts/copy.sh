972c8234fb34b78fde27117afec09affc7b894e3
MatheusGoncalves <math3usgoncalves@protonmail.com>
2020-10-30 10:51:51 -0300

    copy.sh new version

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -4,8 +4,12 @@
 cd ..
 
 ln -sf $PWD/.bashrc $HOME/.bashrc
+ls -l $HOME/.bashrc
 ln -sf $PWD/.vimrc $HOME/.vimrc
+ls -l $HOME/.vimrc 
 ln -sf $PWD/.tmux.conf $HOME/.tmux.conf
+ls -l $HOME/.tmux.conf
+
 cp .git-completion.bash ${HOME}
 cp .gitmessage ${HOME}
 cp .bash_profile ${HOME}
