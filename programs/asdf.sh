f155609ea12783eb940b2ef29341b4c186291bf7
matheus goncalves <matayoos@protonmail.com>
2022-03-09 14:27:24 -0300

    updating asdf.sh

--- programs/asdf.sh
+++ programs/asdf.sh
@@ -4,5 +4,5 @@ git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.1
 
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 
-asdf install nodejs 16.13.1
-asdf global nodejs 16.13.1
+asdf install nodejs 16.14.0
+asdf global nodejs 16.14.0
