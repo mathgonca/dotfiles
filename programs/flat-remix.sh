212fdd9c939666ca9a9502fb0eca90bb631a6510
matheus goncalves <matayoos@protonmail.com>
2022-03-09 13:05:47 -0300

    Fixxing themes path

--- programs/flat-remix.sh
+++ programs/flat-remix.sh
@@ -5,5 +5,7 @@ git clone https://github.com/daniruiz/flat-remix-gtk ~/repos/.flat-remix-gtk
 
 mkdir -p ~/.icons && mkdir -p ~/.themes
 
-cp -r ~/repos/.flat-remix/Flat-Remix* ~/.icons/ && cp -r ~/repos/.flat-remix-gtk/Flat-Remix-GTK* ~/.themes/
+cp -r ~/repos/.flat-remix/Flat-Remix* ~/.icons/ && 
+cp -r ~/repos/.flat-remix-gtk/themes/Flat-Remix-GTK* ~/.themes/
+cp -r ~/repos/.flat-remix-gtk/themes-old/Flat-Remix-GTK* ~/.themes/
 
